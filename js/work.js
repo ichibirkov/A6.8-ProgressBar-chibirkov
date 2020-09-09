78db1569d613951eb0b5265f678b1b999d7a3967
ichibirkov <70270820+ichibirkov@users.noreply.github.com>
2020-09-09 17:51:10 +0300

    Add files via upload

--- js/work.js
+++ js/work.js
@@ -1,34 +1,36 @@
 const progressBar = $("#barstep");
 let width = 25;
 
+
 $(".btn-1").click(function(){
-    width = (width + width * 0.01);
+    width = width + 1
     if (width <= 100) {
     	progressBar.width(width + "%")
     	progressBar.text(width + "%")
-} else {
-	return;
-  	
-	}   
+}   else {
+	    progressBar.width(100 + "%")
+      	progressBar.text(100 + "%")
+	}
 });
 
 $(".btn-3").click(function(){
-    width = (width + width * 0.03);
+    width = width + 3
     if (width <= 100) {
-  	progressBar.width(width + "%")
-    progressBar.text(width + "%")
-} else {
-  	return;
-	}   
+  		progressBar.width(width + "%")
+    	progressBar.text(width + "%")
+}   else {
+    	progressBar.width(100 + "%")
+    	progressBar.text(100 + "%")
+	}
 });
 
 $(".btn-7").click(function(){
-    width = (width  + width * 0.07);
-    if (width <= 100 ) {
-  	progressBar.width(width + "%")
-    progressBar.text(width + "%")
-} else {
-
-  	return ;
-	}   
+    width = width + 7
+    if (width <= 100) {
+    	progressBar.width(width + "%")
+    	progressBar.text(width + "%")
+}   else {
+    	progressBar.width(100 + "%")
+    	progressBar.text(100 + "%")
+  }
 });
